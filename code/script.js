8242c496f0cea923ee77c6a18957def30cd5834d
Irusito <ruben.perez.valdes@gmail.com>
2022-03-09 11:18:29 +0100

    Core del juego

--- code/script.js
+++ code/script.js
@@ -5,12 +5,18 @@
 let n;
 
 function randomN() {
-  n = Math.trunc(Math.random() * 6);
+  n = Math.trunc(Math.random() * 6 + 1);
 }
 
 // defino mis elementos del DOM mediante vairables js
 
-let score0, totalScore0, score1, totalScore1;
+let score0 = 0;
+let score1 = 0;
+let totalScore0 = 0;
+let totalScore1 = 0;
+
+let player0 = true;
+let player1 = false;
 
 const score0Field = document.getElementById('current--0');
 const totalScore0Field = document.getElementById('score--0');
@@ -18,11 +24,17 @@ const totalScore0Field = document.getElementById('score--0');
 const score1Field = document.getElementById('current--1');
 const totalScore1Field = document.getElementById('score--1');
 
+const player0Background = document.querySelector('.player--0');
+const player1Background = document.querySelector('.player--1');
+
 const holdBtn = document.querySelector('.btn--hold');
 const rollBtn = document.querySelector('.btn--roll');
 const resetBtn = document.querySelector('.btn--new');
 
-const imageDice = document.getElementsByClassName('dice');
+const dice = document.querySelector('.dice');
+
+totalScore0Field.textContent = totalScore0;
+totalScore1Field.textContent = totalScore1;
 
 // reset
 
@@ -30,11 +42,14 @@ resetBtn.addEventListener('click', initApp);
 
 function initApp() {
   const INIT_SCORE = 0;
-  totalScore1 = INIT_SCORE;
+  totalScore0 = INIT_SCORE;
   totalScore1 = INIT_SCORE;
   score0 = INIT_SCORE;
   score1 = INIT_SCORE;
-
+  totalScore0Field.textContent = totalScore0;
+  totalScore1Field.textContent = totalScore1;
+  score0Field.textContent = score0;
+  score1Field.textContent = score1;
   randomN();
 }
 
@@ -43,19 +58,45 @@ function initApp() {
 rollBtn.addEventListener('click', rollDice);
 
 function rollDice() {
+  // numero aleatorio
   randomN();
 
   // imagen aleatoria
 
-  imageDice.src = `dice-${n}.png`;
+  const imageDice = `dice-${n}.png`;
+
+  dice.src = imageDice;
 
   if (n === 1) {
-    // pasa el turno
-    score0 = 0;
+    if (player0 === true) {
+      score0 = 0;
+      score0Field.textContent = score0;
+      player1 = true;
+      player0 = false;
+      player0Background.classList.remove('player--active');
+      player1Background.classList.add('player--active');
+    } else {
+      score1 = 0;
+      score1Field.textContent = score1;
+      player0 = true;
+      player1 = false;
+      player0Background.classList.add('player--active');
+      player1Background.classList.remove('player--active');
+    }
   } else {
-    score0 += n;
-    score0Field.textContent = score0;
+    if (player0 === true) {
+      score0 += n;
+      score0Field.textContent = score0;
+    } else {
+      score1 += n;
+      score1Field.textContent = score1;
+    }
   }
+
+  console.log(n);
+  console.log(score0);
+  console.log(player0);
+  console.log(player1);
 }
 
 // hold
@@ -63,7 +104,35 @@ function rollDice() {
 holdBtn.addEventListener('click', holdScore);
 
 function holdScore() {
-  totalScore0 += score0;
-  totalScore0Field.textContent = totalScore0;
-  score0 = 0;
+  if (player0 === true) {
+    totalScore0 += score0;
+    totalScore0Field.textContent = totalScore0;
+    score0 = 0;
+
+    if (totalScore0 >= 100) {
+      player0Background.classList.add('player--winner');
+      //    se desactiva el botón de roll dice
+    }
+
+    player1 = true;
+    player0 = false;
+    player0Background.classList.remove('player--active');
+    player1Background.classList.add('player--active');
+  } else {
+    totalScore1 += score1;
+    totalScore1Field.textContent = totalScore1;
+    score1 = 0;
+
+    if (totalScore1 >= 100) {
+      player1Background.classList.add('player--winner');
+      //    se desactiva el botón de roll dice
+    }
+    player0 = true;
+    player1 = false;
+    player0Background.classList.add('player--active');
+    player1Background.classList.remove('player--active');
+  }
+
+  console.log(player0);
+  console.log(player1);
 }
